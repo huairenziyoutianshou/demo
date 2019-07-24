4ff470c4aced3f1750f5d097efd81848e592f809
huairenziyoutianshou <15010083950@139.com>
2019-07-24 16:56:28 +0800

    本次提交demo2，demo3

---
+++ demo2/src/main/java/com/youyang/demo2/demo2test.java
@@ -0,0 +1,10 @@
+package com.youyang.demo2;
+
+public class demo2test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("demo2test");
+	}
+
+}
---
+++ demo3/src/main/java/com/youyang/demo3/demo3test.java
@@ -0,0 +1,10 @@
+package com.youyang.demo3;
+
+public class demo3test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("demo3test");
+	}
+
+}
